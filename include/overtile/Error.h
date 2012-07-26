de4f233dd7f794bc27f5df71987e07f9c4f0383e
Justin Holewinski <justin.holewinski@gmail.com>
2012-07-26 18:17:59 -0400

    Add OverTile error category/codes

---
+++ include/overtile/Error.h
@@ -0,0 +1,36 @@
+
+
+#ifndef OVERTILE_ERROR_H
+#define OVERTILE_ERROR_H
+
+#include "llvm/Support/system_error.h"
+
+namespace overtile {
+
+// Error handling plumbing
+const llvm::error_category &overtile_category();
+
+struct overtile_error {
+  enum _ {
+    success = 0,
+    invalid_yaml,
+  };
+  _ V_;
+
+  overtile_error(_ v) : V_(v) {}
+  explicit overtile_error(int v) : V_(_(v)) {}
+  operator int() const { return V_; }
+};
+
+inline llvm::error_code make_error_code(overtile_error e) {
+  return llvm::error_code(static_cast<int>(e), overtile_category());
+}
+}
+
+namespace llvm {
+template <> struct is_error_code_enum<overtile::overtile_error> : true_type {};
+template <>
+struct is_error_code_enum<overtile::overtile_error::_> : true_type {};
+}
+
+#endif
---
+++ lib/OTCore/Error.cpp
@@ -0,0 +1,38 @@
+
+#include "overtile/Error.h"
+#include "llvm/Support/ErrorHandling.h"
+
+using namespace llvm;
+
+namespace overtile {
+class _overtile_category : public _do_message {
+public:
+  virtual const char *name() const {
+    return "cuda.elf";
+  }
+
+  virtual std::string message(int ev) const {
+    switch (ev) {
+    case overtile_error::success:
+      return "Success";
+    case overtile_error::invalid_yaml:
+      return "Invalid YAML";
+    default:
+      llvm_unreachable("Invalid overtile_error enumerator");
+    }
+  }
+
+  virtual llvm::error_condition default_error_condition(int ev) const {
+    if (ev == overtile_error::success)
+      return errc::success;
+    else
+      return errc::invalid_argument;
+  }
+};
+
+const llvm::error_category &overtile_category() {
+  static _overtile_category o;
+  return o;
+}
+
+}
