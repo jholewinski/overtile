f09fac936a9cde2af0ac178f61e17025d5b22ccc
Justin Holewinski <justin.holewinski@gmail.com>
2012-09-27 10:00:49 -0400

    Add R script to generate data for time plots

---
+++ tile-search/gen-time-graph.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript
+
+require('utils')
+
+a <- read.csv(file("stdin"))
+a <- subset(a, compute_elapsed > 0.01)
+
+
+# 32x8, 2
+cat('32x8, 2:\n')
+t <- subset(a, x==32 & y==8 & ey==2)
+t <- t[with(t, order(t)),]
+
+for(i in 1:nrow(t)) {
+  row <- t[i,]
+  cat(row$gstencils)
+  cat(',\n')
+}
+
+# 32x8, 6
+cat('32x8, 6:\n')
+t <- subset(a, x==32 & y==8 & ey==6)
+t <- t[with(t, order(t)),]
+
+for(i in 1:nrow(t)) {
+  row <- t[i,]
+  cat(row$gstencils)
+  cat(',\n')
+}
+
+
+# 64x8, 2
+cat('64x8, 2:\n')
+t <- subset(a, x==64 & y==8 & ey==2)
+t <- t[with(t, order(t)),]
+
+for(i in 1:nrow(t)) {
+  row <- t[i,]
+  cat(row$gstencils)
+  cat(',\n')
+}
+
+# 64x8, 6
+cat('64x8, 6:\n')
+t <- subset(a, x==64 & y==8 & ey==6)
+t <- t[with(t, order(t)),]
+
+for(i in 1:nrow(t)) {
+  row <- t[i,]
+  cat(row$gstencils)
+  cat(',\n')
+}
+
+
