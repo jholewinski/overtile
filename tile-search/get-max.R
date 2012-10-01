b3d35b821cf0c416c2408b35be05fd2412f9ade3
Justin Holewinski <justin.holewinski@gmail.com>
2012-10-01 09:49:43 -0400

    Update script

--- tile-search/get-max.R
+++ tile-search/get-max.R
@@ -3,6 +3,19 @@
 require('utils')
 
 a <- read.csv(file("stdin"))
+
 a <- subset(a, compute_elapsed > 0.01)
-print(max(a$gstencils))
+a <- data.frame(a, gstencils_compute_2d=6000*6000*100/1e9/a$compute_elapsed, gstencils_compute_3d=300*300*300*100/1e9/a$compute_elapsed)
+
+cat('GStencils/sec Total: ')
+cat(max(a$gstencils))
+cat('\n')
+
+cat('GStencil/sec Compute-Only (if 2D): ')
+cat(max(a$gstencils_compute_2d))
+cat('\n')
+
+cat('GStencil/sec Compute-Only (if 3D): ')
+cat(max(a$gstencils_compute_3d))
+cat('\n')
 
